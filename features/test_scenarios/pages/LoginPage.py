94f09bfed09a03a122f3dd90f60f9173e10d92e3
najdi013@gmail.com <najdi013@gmail.com>
2021-10-21 10:14:20 +0200

    new changes

--- features/test_scenarios/pages/LoginPage.py
+++ features/test_scenarios/pages/LoginPage.py
@@ -1,31 +1,70 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
-PATH = "C:\Program Files (x86)\Chromedriver.exe"
-
-# locators:
-username = "username"  # id
-password = "password"  # id
-login = "login"  # name
-error = "woocommerce-error"  # class
+from features.test_scenarios.pages.BasePage import BasePage
 
 
-class LoginPage:
+class LoginPage(BasePage):
+    PATH = 'C:\Program Files (x86)\Chromedriver.exe'
+    URL = 'https://biochempro100.pl/moje-konto/'
+    # locators:
+    USERNAME = 'username'  # id
+    PASSWORD = 'password'  # id
+    LOGIN = 'login'  # name
+    ERROR = 'woocommerce-error'  # class
 
     def __init__(self, driver):
-        self.driver = driver
-        driver = webdriver.Chrome(PATH)
+        super().__init__(driver)
+
+    def open_site(self):
+        driver = webdriver.Chrome(self.PATH)
+        driver.get(self.URL)
         driver.maximize_window()
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(15)
 
-    def open_site(self, url):
-        self.driver.get(url)
-
-    def visible_element(self, id_locator):
+    def visible_element_by_id(self, id_locator):
         element = self.driver.find_element(By.ID, id_locator)
-        WebDriverWait(self.driver, 10).until(element.is_displayed)
+        WebDriverWait(self.driver, 15).until(element.is_displayed())
+        if element.is_displayed():
+            print("Element found")
+        else:
+            print("Element not found")
+
+    def visible_element_by_name(self, name_locator):
+        element = self.driver.find_element_by_name(name_locator)
+        if element.is_displayed():
+            print("Element found")
+        else:
+            print("Element not found")
+
+    def visible_element_by_class(self, class_locator):
+        element = self.driver.find_element(By.CLASS_NAME, class_locator)
+        WebDriverWait(self.driver, 15).until(element.is_displayed())
+        if element.is_displayed():
+            print("Element found")
+        else:
+            print("Element not found")
+
+    def find_element(self, id_locator):
+        element = self.driver.find_element(By.ID, id_locator)
         return element.is_displayed()
 
-    def wait(self, time):
+    def clear_form(self, id_locator):
+        element = self.driver.find_element(By.ID, id_locator)
+        element.clear()
+
+    def input_keys(self, id_locator, keys):
+        element = self.driver.find_element(By.ID, id_locator)
+        element.clear()
+        element.send_keys(keys)
+
+    def wait(self, time=5):
         self.driver.implicitly_wait(time)
+
+
+class Locators:
+    # locators:
+    username = 'username'  # id
+    password = 'password'  # id
+    login = 'login'  # name
+    error = 'woocommerce-error'  # class
--- features/test_scenarios/steps/login_steps.py
+++ features/test_scenarios/steps/login_steps.py
@@ -1,51 +1,38 @@
 from behave import *
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from features.test_scenarios.pages.LoginPage import LoginPage, Locators
 
 
 @given('Launch browser')
 def launch_browser(context):
-    path = "C:\Program Files (x86)\Chromedriver.exe"
-    context.driver = webdriver.Chrome(path)
-    context.driver.maximize_window()
-    context.driver.implicitly_wait(5)
+    context.lp = LoginPage(context)
+    context.loc = Locators()
 
 
 @when('Open the https://biochempro100.pl/moje-konto/ website')
 def open_website(context):
-    website = "https://biochempro100.pl/moje-konto/"
-    context.driver.get(website)
+    context.lp.open_site()
 
 
 @then('Login form is visible')
 def visible_login(context):
-    context.username_loc = "username"  # ID
-    context.password_loc = "password"  # ID
-    context.username_el = context.driver.find_element(By.ID, context.username_loc)
-    context.driver.implicitly_wait(5)
-    return context.username_el.is_displayed()
+    context.lp.visible_element_by_name(context.loc.login)
 
 
 @step('Insert username "{username}" and "{password}"')
 def input_user_password(context, username, password):
-    context.password_el = context.driver.find_element(By.ID, context.password_loc)
-    context.username_el.clear()
-    context.password_el.clear()
-    context.username_el.send_keys(username)
-    context.password_el.send_keys(password)
+    context.lp.input_keys(context.loc.username, username)
+    context.lp.input_keys(context.loc.password, password)
 
 
 @step("Click on the login button")
 def login_click(context):
-    context.login_loc = "login"  # name
-    context.login_el = context.driver.find_element(By.NAME, context.login_loc)
+    context.login_el = context.lp.visible_element_by_name(context.loc.login)
     context.login_el.click()
-    context.driver.implicitly_wait(50)
 
 
 @then("Login failed and invalid input data error is displayed")
 def failed_login(context):
-    context.driver.implicitly_wait(5)
     if context.username_el.is_displayed():
         context.driver.implicitly_wait(5)
         context.error_loc = "woocommerce-error"  # class
@@ -68,7 +55,6 @@ def input_password(context):
 
 @then("Login failed and empty username error is displayed")
 def empty_username(context):
-    context.driver.implicitly_wait(5)
     if context.username_el.is_displayed():
         context.driver.implicitly_wait(5)
         context.error_loc = "woocommerce-error"  # class
@@ -86,7 +72,6 @@ def input_username(context):
 
 @then("Login failed and empty password error is displayed")
 def empty_password(context):
-    context.driver.implicitly_wait(5)
     if context.username_el.is_displayed():
         context.driver.implicitly_wait(5)
         context.error_loc = "woocommerce-error"  # class
